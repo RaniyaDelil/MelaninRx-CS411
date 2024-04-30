2350051cb446fd4e6f20d06e0404a086c19f4783
vmulu <144148151+vmulu@users.noreply.github.com>
2024-04-29 20:42:29 -0400

    Update connectDB.js

---
+++ melanin-rx/pages/connectDB.js
@@ -0,0 +1,19 @@
+//connection to database 
+import mongoose from 'mongoose';
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error.message);
+    process.exit(1); // Exit process with failure
+  }
+};
+
+export default connectDB;
